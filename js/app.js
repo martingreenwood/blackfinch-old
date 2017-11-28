132190a611b325ed3b66f989e0b897fcddc20ce0
1 <martin@pixelpudu.com>
2017-11-28 10:12:20 +0000

    fix

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@
  * @package Blackfinch
  */
 
+function resizeIframe(obj) {
+    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+  }
+  
 function changeMenu(){
 	$ = jQuery;
 	var offset = $("#masthead").offset();
